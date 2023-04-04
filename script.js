ee5c669131722a5ea9b1a6f8e71a816e8ea0d424
Michel Maciel <michel.asm@hotmail.com>
2023-04-04 14:55:37 -0300

    getting the values of all inputs, adding all parameters and variables, and creating an event to hide the input

--- script.js
+++ script.js
@@ -1,50 +1,113 @@
-		// show a message with a type of the input
-		function showMessage(input, message, type) {
-			// get the small element and set the message
-			const msg = input.parentNode.querySelector("small");
-			msg.innerText = message;
-			// update the class for the input
-			input.className = type ? "form-control" : "form-control is-invalid";
-			return type;
+	// show a message with a type of the input
+	function showMessage(input, message, type) {
+		// get the small element and set the message
+		const msg = input.parentNode.querySelector("small");
+		msg.innerText = message;
+		// update the class for the input
+		input.className = type ? "form-control" : "form-control is-invalid";
+		return type;
+	}
+	function showError(input, message) {
+		return showMessage(input, message, false);
+	}
+	function showSuccess(input) {
+		return showMessage(input, "", true);
+	}
+	function hasValue(input, message) {
+		if (input.value.trim() === "") {
+			return showError(input, message);
 		}
-		function showError(input, message) {
-			return showMessage(input, message, false);
-		}
-		function showSuccess(input) {
-			return showMessage(input, "", true);
-		}
-		function hasValue(input, message) {
-			if (input.value.trim() === "") {
-				return showError(input, message);
+		return showSuccess(input);
+	}
+
+	// show email name in input result
+	function showFeatureName(feature, office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew) {
+
+		if (feature == "EMAIL"){
+			let result = office + "_" + type + "_" + category;
+			if (subCategory == "Novo") {
+				result += "_" + subCategoryNew.trim().replace("_", "-").replace(" ", "-");
+			} else {
+				result += "_" + subCategory;
 			}
-			return showSuccess(input);
+			if (posSignupJourney == "Novo") {
+				result += "_" + posSignupJourneyNew.trim().replace("_", "-").replace(" ", "-");
+			} else {
+				result += "_" + posSignupJourney;
+			}
+			if (contentType == "Novo") {
+				result += "_" + contentTypeNew.trim().replace("_", "-").replace(" ", "-");
+			}
+			if (contentType !== "" && contentType !== "Novo") {
+				result += "_" + contentType;
+			}
+			if (emailPsychology !== "") {
+				result += "_" + emailPsychology;
+			}
+			if (funnel !== "") {
+				result += "_" + funnel;
+			}
+			return result.toUpperCase();
 		}
 
-		// show email name in input result
-		function showFeatureName(feature, country, type, team, campaignName) {
+	}
+	
+	const form = document.querySelector("#signup");
+	const campaignNameValid = true;
 
-			if (feature == "EMAIL"){
-				// get the small element and set the message
-				const result = country + "_" + type + "_" + team + "-" + campaignName
-				return result.toUpperCase();
-			}
+	form.addEventListener("submit", function (event) {
+		// stop form submission
+		event.preventDefault();
+
+		// if valid, submit the form.
+		if (campaignNameValid) {
+			let office = form.querySelector("#office").value;
+			let type = form.querySelector("#type").value;
+			let category = form.querySelector("#category").value;
+			let subCategory = form.querySelector("#sub-category").value;
+			let posSignupJourney = form.querySelector("#pos-signup-journey").value;
+			let contentType = form.querySelector("#content-type").value;
+			let emailPsychology = form.querySelector("#email-psychology").value;
+			let funnel = form.querySelector("#funnel").value;
+			let subCategoryNew = form.querySelector("#sub-category-new").value.trim().replace("_", "-").replace(" ", "-");
+			let posSignupJourneyNew = form.querySelector("#pos-signup-journey-new").value.trim().replace("_", "-").replace(" ", "-");
+			let contentTypeNew = form.querySelector("#content-type-new").value.trim().replace("_", "-").replace(" ", "-");
+
+			//returning the responses
+			form.elements["emailNameResult"].value = showFeatureName("EMAIL", office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew);
 		}
-    
-		const form = document.querySelector("#signup");
-		const CAMPAIGN_REQUIRED = "Please enter your campaign name";
+	});
 
-		form.addEventListener("submit", function (event) {
-			// stop form submission
-			event.preventDefault();
-			// validate the form
-			let campaignNameValid = hasValue(form.elements["campaign-name"], CAMPAIGN_REQUIRED);
-			// if valid, submit the form.
-			if (campaignNameValid) {
-				let country = form.querySelector("#country").value;
-				let team = form.querySelector("#team").value;
-				let campaignName = form.elements["campaign-name"].value.trim().replace("_", "-").replace(" ", "-");
-				let type = form.querySelector("#type").value;
-				//returning the responses
-				form.elements["emailNameResult"].value = showFeatureName("EMAIL", country, type, team, campaignName);
-			}
-		});
+let subCategoryNewDIV = form.querySelector("[data-set-sub-category-new]");
+let subCategorySelect = form.querySelector("#sub-category");
+
+let posSignupJourneyNewDIV = form.querySelector("[data-set-pos-signup-journey-new]");
+let posSignupJourneySelect = form.querySelector("#pos-signup-journey");
+
+let contentTypeNewDIV = form.querySelector("[data-set-content-type-new]");
+let contentTypeSelect = form.querySelector("#content-type");
+
+
+subCategorySelect.addEventListener("change", function() {
+if (subCategorySelect.value === "Novo") {
+	subCategoryNewDIV.style.display = "block";
+} else {
+	subCategoryNewDIV.style.display = "none";
+}
+});
+
+posSignupJourneySelect.addEventListener("change", function() {
+if (posSignupJourneySelect.value === "Novo") {
+	posSignupJourneyNewDIV.style.display = "block";
+} else {
+	posSignupJourneyNewDIV.style.display = "none";
+}
+});
+
+contentTypeSelect.addEventListener("change", function() {
+if (contentTypeSelect.value === "Novo") {
+	contentTypeNewDIV.style.display = "block";
+} else {
+	contentTypeNewDIV.style.display = "none";
+}
+});
