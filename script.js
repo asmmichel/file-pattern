d6cad1c115b10411e0fb17e8c540005ee3020d75
Michel Maciel <michel.asm@hotmail.com>
2023-04-04 14:34:52 -0300

    keeping only the email return to be worked on

--- script.js
+++ script.js
@@ -28,41 +28,6 @@
 				const result = country + "_" + type + "_" + team + "-" + campaignName
 				return result.toUpperCase();
 			}
-			else if (feature == "DE"){
-				// get the small element and set the message
-				const result = "TB_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "FILTER"){
-				// get the small element and set the message
-				const result = "FILTER_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "SQL"){
-				// get the small element and set the message
-				const result = "QR_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "AUT"){
-				// get the small element and set the message
-				const result = "AUT_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "IMPORT"){
-				// get the small element and set the message
-				const result = "IMPORT_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "DATA_EXT"){
-				// get the small element and set the message
-				const result = "DE_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
-			else if (feature == "FT"){
-				// get the small element and set the message
-				const result = "FT_" + country.replace("-","_") + "_" + type + "_" + team + "_" + campaignName.replace("-","_").replace(" ","_")
-				return result.toUpperCase();
-			}
 		}
     
 		const form = document.querySelector("#signup");
@@ -71,7 +36,6 @@
 		form.addEventListener("submit", function (event) {
 			// stop form submission
 			event.preventDefault();
-
 			// validate the form
 			let campaignNameValid = hasValue(form.elements["campaign-name"], CAMPAIGN_REQUIRED);
 			// if valid, submit the form.
@@ -81,13 +45,6 @@
 				let campaignName = form.elements["campaign-name"].value.trim().replace("_", "-").replace(" ", "-");
 				let type = form.querySelector("#type").value;
 				//returning the responses
-				form.elements["emailNameResult"].value        = showFeatureName("EMAIL", country, type, team, campaignName);
-				form.elements["deNameResult"].value           = showFeatureName("DE", country, type, team, campaignName);
-				form.elements["filterNameResult"].value       = showFeatureName("FILTER", country, type, team, campaignName);
-				form.elements["sqlNameResult"].value          = showFeatureName("SQL", country, type, team, campaignName);
-				form.elements["automationNameResult"].value   = showFeatureName("AUT", country, type, team, campaignName);
-				form.elements["importNameResult"].value       = showFeatureName("IMPORT", country, type, team, campaignName);
-				form.elements["dataExtractNameResult"].value  = showFeatureName("DATA_EXT", country, type, team, campaignName);
-				form.elements["fileTransferNameResult"].value = showFeatureName("FT", country, type, team, campaignName);
+				form.elements["emailNameResult"].value = showFeatureName("EMAIL", country, type, team, campaignName);
 			}
 		});
