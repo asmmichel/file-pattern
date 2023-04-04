f02359555b7b643cdb82078ef16e82503faae874
Michel Maciel <michel.asm@hotmail.com>
2023-04-04 17:56:26 -0300

    creating an object for the form values and using ternary operator in the concatenation

--- script.js
+++ script.js
@@ -23,36 +23,24 @@
 	
 
 	// show email name in input result
-	function showFeatureName(feature, office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew) {
+	function showFeatureName(feature, featureData) {
+    const { office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew } = featureData;
 
-		if (feature == "EMAIL"){
-			let result = office + "_" + type + "_" + category;
-			if (subCategory == "Novo") {
-				result += "_" + subCategoryNew;
-			} else {
-				result += "_" + subCategory;
-			}
-			if (posSignupJourney == "Novo") {
-				result += "_" + posSignupJourneyNew;
-			} else {
-				result += "_" + posSignupJourney;
-			}
-			if (contentType == "Novo") {
-				result += "_" + contentTypeNew;
-			}
-			if (contentType !== "" && contentType !== "Novo") {
-				result += "_" + contentType;
-			}
-			if (emailPsychology !== "") {
-				result += "_" + emailPsychology;
-			}
-			if (funnel !== "") {
-				result += "_" + funnel;
-			}
-			return result.toUpperCase().trim().replaceAll(" ", "-");
-		}
+    if (feature === "EMAIL") {
+        let result = `${office}_${type}_${category}`;
 
-	}
+        result += subCategory === "Novo" ? `_${subCategoryNew}` : `_${subCategory}`;
+        result += posSignupJourney === "Novo" ? `_${posSignupJourneyNew}` : `_${posSignupJourney}`;
+        result += contentType === "Novo" ? `_${contentTypeNew}` : (contentType !== "" ? `_${contentType}` : "");
+        result += emailPsychology !== "" ? `_${emailPsychology}` : "";
+        result += funnel !== "" ? `_${funnel}` : "";
+
+        return result.toUpperCase().trim().replaceAll(" ", "-");
+    }
+}
+
+
+	
 	
 	const form = document.querySelector("#signup");
 	const campaignNameValid = true;
@@ -62,22 +50,29 @@
 		event.preventDefault();
 
 		// if valid, submit the form.
-		if (campaignNameValid) {
-			let office = form.querySelector("#office").value;
-			let type = form.querySelector("#type").value;
-			let category = form.querySelector("#category").value;
-			let subCategory = form.querySelector("#sub-category").value;
-			let posSignupJourney = form.querySelector("#pos-signup-journey").value;
-			let contentType = form.querySelector("#content-type").value;
-			let emailPsychology = form.querySelector("#email-psychology").value;
-			let funnel = form.querySelector("#funnel").value;
-			let subCategoryNew = form.querySelector("#sub-category-new").value;
-			let posSignupJourneyNew = form.querySelector("#pos-signup-journey-new").value;
-			let contentTypeNew = form.querySelector("#content-type-new").value;
 
-			//returning the responses
-			form.elements["emailNameResult"].value = showFeatureName("EMAIL", office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew);
+		console.log(form);
+
+		if (campaignNameValid) {
+			let featureData = {
+				office: form.querySelector("#office").value,
+				type: form.querySelector("#type").value,
+				category: form.querySelector("#category").value,
+				subCategory: form.querySelector("#sub-category").value,
+				posSignupJourney: form.querySelector("#pos-signup-journey").value,
+				contentType: form.querySelector("#content-type").value,
+				emailPsychology: form.querySelector("#email-psychology").value,
+				funnel: form.querySelector("#funnel").value,
+				subCategoryNew: form.querySelector("#sub-category-new").value,
+				posSignupJourneyNew: form.querySelector("#pos-signup-journey-new").value,
+				contentTypeNew: form.querySelector("#content-type-new").value
+			};
+		
+			// returning the responses
+			form.elements["emailNameResult"].value = showFeatureName("EMAIL", featureData);
 		}
+		
+		
 	});
 
 
