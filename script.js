fdb27b744d84146ec916b57307e327867b162021
Michel Maciel <michel.asm@hotmail.com>
2023-04-21 16:19:04 -0300

    melhorando varias partes, deixando varias funcoes dinamicas

--- script.js
+++ script.js
@@ -1,219 +1,213 @@
 	// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "form-control" : "form-control is-invalid";
-	return type;
-}
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
+	function showMessage(input, message, type) {
+		// get the small element and set the message
+		const msg = input.parentNode.querySelector("small");
+		msg.innerText = message;
+		// update the class for the input
+		input.className = type ? "form-control" : "form-control is-invalid";
+		return type;
 	}
-	return showSuccess(input);
-}
-
-
-
-function extractFormData(form) {
-	return {
-		office: form.querySelector("#office").value,
-		type: form.querySelector("#type").value,
-		category: form.querySelector("#category").value,
-		subCategory: form.querySelector("#sub-category").value,
-		posSignupJourney: form.querySelector("#pos-signup-journey").value,
-		contentType: form.querySelector("#content-type").value,
-		emailPsychology: form.querySelector("#email-psychology").value,
-		funnel: form.querySelector("#funnel").value,
-		subCategoryNew: form.querySelector("#sub-category-new").value,
-		emptyWarningDivSubCategory: form.querySelector("[data-set-sub-category-new] div"),
-		inputBoxSubCategory: form.querySelector("[data-set-sub-category-new] input"),
-		posSignupJourneyNew: form.querySelector("#pos-signup-journey-new").value,
-		emptyWarningDivPosSignupJourney: form.querySelector("[data-set-pos-signup-journey-new] div"),
-		inputBoxPosSignupJourney: form.querySelector("[data-set-pos-signup-journey-new] input"),
-		contentTypeNew: form.querySelector("#content-type-new").value
-	};
-}
-
-function showFeatureName(prefix, featureData, event) {
-	const { office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew } = featureData;
-
-	let result = `${prefix}${office}_${type}_${category}`;
-
-	if (subCategory === "Novo")  result += `_${subCategoryNew}` 
-	else result += `_${subCategory}`;
-
-	if (posSignupJourney === "Novo") result += `_${posSignupJourneyNew}`;
-	else	result += `_${posSignupJourney}`;
-
-	if (contentType === "Novo") result += `_${contentTypeNew}`;
-	else if (contentType !== "") result += `_${contentType}`;
-
-	if (emailPsychology !== "") result += `_${emailPsychology}`;
+	function showError(input, message) {
+		return showMessage(input, message, false);
+	}
+	function showSuccess(input) {
+		return showMessage(input, "", true);
+	}
+	function hasValue(input, message) {
+		if (input.value.trim() === "") {
+			return showError(input, message);
+		}
+		return showSuccess(input);
+	}
 	
-	if (funnel !== "") result += `_${funnel}`;
-
-	if(event.submitter.name === 'date') return showFeatureNameWithDate(result);
-
-	return result.toUpperCase().trim().replaceAll(" ", "-");
-}
-
-function showFeatureNameWithDate(result) {
-	const date = new Date();
-	const year = String(date.getFullYear()).slice(-2);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-	result += `-${year}${month}${day}`;
-	return result.toUpperCase().trim().replaceAll(" ", "-");
-}
-
-function showNewTextInputs(newTextInputsOBJ) {
-  newTextInputsOBJ.forEach(inputOBJ => {
-    inputOBJ.select.addEventListener("change", () => {
-      inputOBJ.div.style.display = inputOBJ.select.value === "Novo" ? "block" : "none";
-    });
-  });
-}
-
-function checkTheLengthAndSendValues(formElement, result) {
-  if (result.length >= 100) {
-    return true;
-  } else {
-		form.elements[formElement].value = result;
-		return false;
+	
+	
+	function extractFormData(form) {
+		return {
+			office: form.querySelector("#office").value,
+			type: form.querySelector("#type").value,
+			category: form.querySelector("#category").value,
+			subCategory: form.querySelector("#sub-category").value,
+			posSignupJourney: form.querySelector("#pos-signup-journey").value,
+			contentType: form.querySelector("#content-type").value,
+			emailPsychology: form.querySelector("#email-psychology").value,
+			funnel: form.querySelector("#funnel").value,
+			subCategoryNew: form.querySelector("#sub-category-new").value,
+			posSignupJourneyNew: form.querySelector("#pos-signup-journey-new").value,
+			contentTypeNew: form.querySelector("#content-type-new").value
+		};
 	}
-}
-
-function checkIfNewTextInputsAreEmpty(featureData) {
-	const { subCategory, subCategoryNew, emptyWarningDivSubCategory, inputBoxSubCategory, posSignupJourney, posSignupJourneyNew , emptyWarningDivPosSignupJourney, inputBoxPosSignupJourney, contentTypeNew} = featureData;
-	const emptyCheckerArray = []
-
-	if (subCategory === "Novo" && subCategoryNew === "") {
-		emptyWarningDivSubCategory.style.display = "block"
-		inputBoxSubCategory.style.border = "1px solid red";
-		emptyCheckerArray.push(true)
-	} else {
-		emptyWarningDivSubCategory.style.display = "none"
-		inputBoxSubCategory.style.border = "none";
-		emptyCheckerArray.push(false)
-	}
-
-	if (posSignupJourney === "Novo" && posSignupJourneyNew === "") {
-		emptyWarningDivPosSignupJourney.style.display = "block"
-		inputBoxPosSignupJourney.style.border = "1px solid red";
-		emptyCheckerArray.push(true)
-	} else {
-		emptyWarningDivPosSignupJourney.style.display = "none"
-		inputBoxPosSignupJourney.style.border = "none";
-		emptyCheckerArray.push(false)
-	}
-
-	return emptyCheckerArray;
-}
-
-function showBoxWarningAndOpenGmail(divsArray) {
-	divsArray.forEach((div) => {
-		const span = div.querySelector('span')
+	
+	function showFeatureName(prefix, featureData, event) {
+		const { office, type, category, subCategory, posSignupJourney, contentType, emailPsychology, funnel, subCategoryNew, posSignupJourneyNew, contentTypeNew } = featureData;
+	
+		let result = `${prefix}${office}_${type}_${category}`;
+	
+		if (subCategory === "Novo")  result += `_${subCategoryNew}` 
+		else result += `_${subCategory}`;
+	
+		if (posSignupJourney === "Novo") result += `_${posSignupJourneyNew}`;
+		else	result += `_${posSignupJourney}`;
+	
+		if (contentType === "Novo") result += `_${contentTypeNew}`;
+		else if (contentType !== "") result += `_${contentType}`;
+	
+		if (contentType === "Novo" && contentTypeNew === "")  result = result.slice(0, -1);
+	
+		if (emailPsychology !== "") result += `_${emailPsychology}`;
 		
-		div.addEventListener('mouseover', () => {
-			span.style.display = 'block';
+		if (funnel !== "") result += `_${funnel}`;
+	
+		if(event.submitter.name === 'date') return showFeatureNameWithDate(result);
+	
+		return result.toUpperCase().trim().replaceAll(" ", "-");
+	}
+	
+	function showFeatureNameWithDate(result) {
+		const date = new Date();
+		const year = String(date.getFullYear()).slice(-2);
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+		result += `-${year}${month}${day}`;
+		return result.toUpperCase().trim().replaceAll(" ", "-");
+	}
+	
+	function showNewTextInputs(newTextInputsOBJ) {
+		newTextInputsOBJ.forEach(inputOBJ => {
+			inputOBJ.select.addEventListener("change", () => {
+				inputOBJ.div.style.display = inputOBJ.select.value === "Novo" ? "block" : "none";
+			});
 		});
-		div.addEventListener('mouseout', () => {
-				span.style.display = 'none';
-		});
-
-		const emailsDivs = document.querySelectorAll('div a.email-chat')
-				
-		emailsDivs.forEach((email) => {
-			const ariaLabel = email.getAttribute('aria-label');
-			email.addEventListener('click', (event) => {
-				event.preventDefault();
-				window.open(ariaLabel, '_blank');
+	}
+	
+	function checkTheLengthAndSendValues(formElement, result) {
+		if (result.length >= 100) {
+			return true;
+		} else {
+			form.elements[formElement].value = result;
+			return false;
+		}
+	}
+	
+	function checkIfNewTextInputsAreEmpty(newInputsOBJ) {
+		const emptyCheckerArray = []
+	
+		newInputsOBJ.forEach((newElement) => {
+			const { formElement, formNewElement } = newElement;
+	
+			const emptyWarningDiv = formNewElement.querySelector('.empty-warning')
+			const formNewElementInput = formNewElement.querySelector('.never-empty');
+	
+			if (formElement.value === "Novo" && formNewElementInput.value === "") {
+				emptyWarningDiv.style.display = "block"
+				formNewElementInput.style.border = "1px solid red";
+				emptyCheckerArray.push(true)
+			} else {
+				emptyWarningDiv.style.display = "none"
+				formNewElementInput.style.border = "none";
+				emptyCheckerArray.push(false)
+			}
+		})
+		return emptyCheckerArray;
+	}
+	
+	function showBoxWarningAndOpenGmail(newInputsOBJ) {
+		newInputsOBJ.forEach((newElement) => {
+			const { formNewElement } = newElement;
+			const span = formNewElement.querySelector('span')
+			formNewElement.addEventListener('mouseover', () => span.style.display = 'block');
+			formNewElement.addEventListener('mouseout', () => span.style.display = 'none');
+	
+			const emailsDivs = formNewElement.querySelectorAll('a.email-chat')
+			emailsDivs.forEach((email) => {
+				const ariaLabel = email.getAttribute('aria-label');
+				email.addEventListener('click', (event) => {
+					event.preventDefault();
+					window.open(ariaLabel, '_blank');
+				})
 			})
 		})
-	})
-}
-
-
-const form = document.querySelector("#signup");
-
-const formElementsAndPrefix = [
-	{
-		formElement: "emailNameResult",
-		prefix: ""
-	},
-	{
-		formElement: "deNameResult",
-		prefix: "DE_"
-	},
-	{
-		formElement: "filterNameResult",
-		prefix: "FILTER_"
-	},
-	{
-		formElement: "sqlNameResult",
-		prefix: "SQL_"
-	},
-	{
-		formElement: "automationNameResult",
-		prefix: "AUT_"
-	},
-	{
-		formElement: "importNameResult",
-		prefix: "IMPORT_"
-	},
-	{
-		formElement: "dataExtractNameResult",
-		prefix: "DATA_EXT_"
-	},
-	{
-		formElement: "fileTransferNameResult",
-		prefix: "FT_"
 	}
-];
-
-showNewTextInputs([
-  {
-    select: form.querySelector("#sub-category"),
-    div: form.querySelector("[data-set-sub-category-new]")
-  },
-  {
-    select: form.querySelector("#pos-signup-journey"),
-    div: form.querySelector("[data-set-pos-signup-journey-new]")
-  },
-  {
-    select: form.querySelector("#content-type"),
-    div: form.querySelector("[data-set-content-type-new]")
-  }
-]);
-
-showBoxWarningAndOpenGmail([
-	form.querySelector("[data-set-sub-category-new]"),
-	form.querySelector("[data-set-pos-signup-journey-new]")
-]);
-
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
-	const featureData = extractFormData(form);
-	const resultsLenghtArray = [];
-
-	formElementsAndPrefix.forEach(({ formElement, prefix }) => {
-		let result = showFeatureName(prefix, featureData, event);
-		const emptyCheckerArray = checkIfNewTextInputsAreEmpty(featureData)
-		if(emptyCheckerArray.includes(true)) result = '';
-		resultsLenghtArray.push(checkTheLengthAndSendValues(formElement, result));
+	
+	const form = document.querySelector("#signup");
+	
+	const formElementsAndPrefix = [
+		{
+			formElement: "emailNameResult",
+			prefix: ""
+		},
+		{
+			formElement: "deNameResult",
+			prefix: "DE_"
+		},
+		{
+			formElement: "filterNameResult",
+			prefix: "FILTER_"
+		},
+		{
+			formElement: "sqlNameResult",
+			prefix: "SQL_"
+		},
+		{
+			formElement: "automationNameResult",
+			prefix: "AUT_"
+		},
+		{
+			formElement: "importNameResult",
+			prefix: "IMPORT_"
+		},
+		{
+			formElement: "dataExtractNameResult",
+			prefix: "DATA_EXT_"
+		},
+		{
+			formElement: "fileTransferNameResult",
+			prefix: "FT_"
+		}
+	];
+	
+	const newInputsOBJ = [
+		{
+			formElement: form.querySelector("#sub-category"),
+			formNewElement: form.querySelector("[data-set-sub-category-new]")
+		},
+		{
+			formElement: form.querySelector("#pos-signup-journey"),
+			formNewElement: form.querySelector("[data-set-pos-signup-journey-new]")
+		},
+	];
+	
+	showNewTextInputs([
+		{
+			select: form.querySelector("#sub-category"),
+			div: form.querySelector("[data-set-sub-category-new]")
+		},
+		{
+			select: form.querySelector("#pos-signup-journey"),
+			div: form.querySelector("[data-set-pos-signup-journey-new]")
+		},
+		{
+			select: form.querySelector("#content-type"),
+			div: form.querySelector("[data-set-content-type-new]")
+		}
+	]);
+	
+	showBoxWarningAndOpenGmail(newInputsOBJ);
+	
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const featureData = extractFormData(form);
+		const resultsLenghtArray = [];
+		
+		formElementsAndPrefix.forEach(({ formElement, prefix }) => {
+			let result = showFeatureName(prefix, featureData, event);
+			const emptyCheckerArray = checkIfNewTextInputsAreEmpty(newInputsOBJ)
+			if(emptyCheckerArray.includes(true)) result = '';
+			resultsLenghtArray.push(checkTheLengthAndSendValues(formElement, result));
+		});
+	
+		if (resultsLenghtArray.every((result) => result === true)) {
+			alert('Erro, o nome ultrapassou 100 caracteres, retire algumas partes opcionais ou diminua os termos!');
+		}
 	});
-
-	if (resultsLenghtArray.every((result) => result === true)) {
-		alert('Erro, o nome ultrapassou 100 caracteres, retire algumas partes opcionais ou diminua os termos!');
-	}
-});
-
-
+	
